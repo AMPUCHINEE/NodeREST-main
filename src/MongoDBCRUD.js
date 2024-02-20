956d8a6532ba2c458c73651810d69b9ffc63744c
AMPUCHINEE <s6506021620113@email.kmutnb.ac.th>
2024-02-20 10:18:26 +0700

    ok

--- src/MongoDBCRUD.js
+++ src/MongoDBCRUD.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 mongoose.connect(
-    "mongodb://admin:ADGiia23368@node58238-nodeam.proen.app.ruk-com.cloud/Books",
+    "mongodb://admin:ADGiia23368@node58238-nodeam.proen.app.ruk-com.cloud:11597",
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
